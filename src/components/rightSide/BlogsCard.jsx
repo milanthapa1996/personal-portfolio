d13427189bf0cd36b349431e7fc334d8b89b1567
milanThapa1996 <tmilan0604@gmail.com>
2022-08-19 11:21:53 +0545

    blog updated

--- src/components/rightSide/BlogsCard.jsx
+++ src/components/rightSide/BlogsCard.jsx
@@ -1,34 +1,26 @@
-import React from "react";
-import { HiOutlineFolderOpen } from "react-icons/hi";
-import {
-  AiOutlineBranches,
-  AiOutlineRocket,
-  AiOutlineStar,
-} from "react-icons/ai";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 import { BsFillCircleFill, BsCodeSlash, BsBroadcast } from "react-icons/bs";
 
 const BlogsCard = () => {
-  const projects = [
-    {
-      name: "Project 1",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-      link: "#",
-      startCount: 4,
-      forkCount: 12,
-      langauage: "JavaScript",
-      color: "yellow",
-    },
-    {
-      name: "Project 2",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit.",
-      link: "#",
-      startCount: 4,
-      forkCount: 12,
-      langauage: "Python",
-      color: "blue",
+  const [blogs, setBlogs] = useState([]);
+
+  const fetchBlogs = async () => {
+    try {
+      const res = await axios.get(
+        `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@tmilan0604`
+      );
+      console.log(res.data.items);
+      setBlogs(res.data.items);
+    } catch (error) {
+      console.log(error);
     }
-  ];
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
 
   return (
     <div className="bg-gray-100 dark:bg-slate-800 rounded-lg shadow-2xl p-5">
@@ -39,36 +31,40 @@ const BlogsCard = () => {
           </h1>
         </div>
         <div className="flex flex-row justify-center items-center">
-          <a href="#" className="underline text-gray-500 dark:text-gray-200 hover:text-blue-500">
+          <a
+            href="#"
+            className="underline text-gray-500 dark:text-gray-200 hover:text-blue-500"
+          >
             see all
           </a>
         </div>
       </div>
       <div className="grid grid-cols-1 gap-4 mt-4">
-        {projects.map((project, index) => (
-          <div className="bg-white dark:bg-slate-700 rounded-xl shadow-md overflow-hidden" key={index}>
+        {blogs.map((blog, index) => (
+          <div
+            className="bg-white dark:bg-slate-700 rounded-xl shadow-md overflow-hidden"
+            key={index}
+          >
             <div className="md:flex">
               <div className="md:shrink-0">
                 <img
                   className="h-48 w-full object-cover md:h-full md:w-48"
-                  src="https://picsum.photos/seed/picsum/200/200"
+                  src={blog.thumbnail}
                   alt="Man looking at item at a store"
                 />
               </div>
               <div className="p-8">
                 <div className="uppercase tracking-wide text-sm text-indigo-500 dark:text-gray-100 font-semibold">
-                  Case study
+                  Author: {blog.author}
                 </div>
                 <a
-                  href="#"
+                  href={blog.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className="block mt-1 text-lg leading-tight font-medium text-black dark:text-gray-300 hover:underline"
                 >
-                  Finding customers for your new business
+                  {blog.title}
                 </a>
-                <p className="mt-2 text-slate-500 dark:text-gray-400">
-                  Getting a new business off the ground is a lot of hard work.
-                  Here are five ideas you can use to find your first customers.
-                </p>
               </div>
             </div>
           </div>
